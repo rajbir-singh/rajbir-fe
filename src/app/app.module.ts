af5f31913bb36034562997032863633b1f2312ad
hurb <rajbirs799@gmail.com>
2017-10-16 15:18:28 +0530

    -

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -81,7 +81,7 @@ import { IconsComponent }   from './icons/icons.component';
 import { MapsComponent }   from './maps/maps.component';
 import { NotificationsComponent }   from './notifications/notifications.component';
 import { UpgradeComponent }   from './upgrade/upgrade.component';
-import { UserResolver }   from './user/UserResolver';
+import { UserResolver }   from './user/userResolver';
 import { UserService }   from './services/user.service';
 import { GroupService }   from './services/GroupService';
 import { GroupListResolver }   from './group-list/group.list.resolver';
